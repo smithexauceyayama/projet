5771d2e35b810c027125cd86d739e669b30ddf2f
smithexauceyayama <exauceyayama@gmail.com>
2025-06-24 10:53:16 +0100

    modification js et style
    j'ai fait en sorte que lorsqu'on clique sur le bouton menu, la variable boton prend le style display none et lorsqu'on ferme display block

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ const FermerMenu = document.getElementById('navbar-clicable');
 
 bouton.addEventListener('click', function() {
     menu.style.display = 'flex';
+    bouton.style.display = 'none';
 });
 FermerMenu.addEventListener('click', function() {
     menu.style.display = 'none';
+    bouton.style.display = 'block';
 });
